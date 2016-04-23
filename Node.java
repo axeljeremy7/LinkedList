01626c3f1f6ffb1a3da3fa5b75d443131df6e8a1
Axel Jeremy Solano <axel.jeremy.7@gmail.com>
2016-04-23 00:22:18 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,46 +1,54 @@
+public class Node<E> implements INode<E> {
+	private E e;
+	private INode<E> n;
+	/**
+	 * a constructor
+	 * @param i whatever object
+	 * @param p  for the next object
+	 */
+	
+	public Node(E i, INode<E> p){
+		this.e=i;
+		this.n=p;
+	}
+	/**
+	 * a method to return the type of e, the type of object
+	 * @return e as any kind of object
+	 * Returns the data stored in this node.
+	 */
+	@Override
+	public E getData() {
+		// TODO Auto-generated method stub
+		return e;
+	}
+	/**
+	 * a method to be setter for data for this node
+	 * @param data the object to set and it can be any kind of object
+	 */
 
-public class Node {
-	Node next;
-	Object data;
+	@Override
+	public void setData(E data) {
+		e=data;
+		
+	}
+	/**
+	 * a method to returns the node next to this node
+	 * @return n as an object
+	 */
 
-	public Node(Object dataValue) {
-
-		next = null;
-
-		data = dataValue;
-
+	@Override
+	public INode<E> getNext() {
+		// TODO Auto-generated method stub
+		return n;
 	}
 
-	public Node(Object dataValue, Node nextValue) {
+	/**
+	 * @param next  an object
+	 * a method to set node received as the next node to this node.
 
-		next = nextValue;
-
-		data = dataValue;
-
+	 */
+	@Override
+	public void setNext(INode<E> next) {
+		n=next;
+		
 	}
-
-	public Object getData() {
-
-		return data;
-
-	}
-
-	public void setData(Object dataValue) {
-
-		data = dataValue;
-
-	}
-
-	public Node getNext() {
-
-		return next;
-
-	}
-
-	public void setNext(Node nextValue) {
-
-		next = nextValue;
-
-	}
-
-}
