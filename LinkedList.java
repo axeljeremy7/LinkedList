889b0ff63dc9c9c0fe5b8e7d241e9c024a0f9bdb
Axel Jeremy Solano <axel.jeremy.7@gmail.com>
2016-04-23 00:24:27 -0400

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,18 +1,22 @@
-
-public class LinkedList {
-	Node head; 
-	int size;
-
-	public void add(Object data) {
+public class LinkedList<T> implements IList<T> {
+	private INode<T> head;
+	private int size = 0;
+	
+	/**
+	 * method for adding to a linkedlist
+	 * @param e is a generic type to work with any object
+	 */
+	@Override
+	public void add(T e) {
 		if (head == null) {
-			head = new Node(data, null);
+			head = new Node<T>(e, null);
 		} else {
-			Node temp = new Node(data,null);
+			INode<T> temp = new Node<T>(e, null);
 			if (head.getNext() == null) {
-				Node current = head;
+				INode<T> current = head;
 				current.setNext(temp);
 			} else {
-				Node current = head;
+				INode<T> current = head;
 				if (current != null) {
 					while (current.getNext() != null) {
 						current = current.getNext();
@@ -22,13 +26,21 @@ public class LinkedList {
 			}
 		}
 		size++;
+		
 	}
-
-	public void add(Object data, int index) throws IndexOutOfBoundsException {
-		Node temp = new Node(data,null);
-		Node current = head;
+	/**
+	 * method of adding with the specification of the index
+	 * @param index is an integer
+	 * @param e is generic type of object
+	  * @throws   IndexOutOfBoundsException : if is out of index 
+	 */
+	@Override
+	public void add(int index, T e) throws IndexOutOfBoundsException {
+		
+		INode <T> temp = new Node <T>(e,null);
+		INode <T> current = head;
 		if (index == 0) {
-			head = new Node(data, current);
+			head = new Node <T>(e, current);
 		} else {
 			if (current != null) {
 				if (index <= size - 1){
@@ -41,10 +53,57 @@ public class LinkedList {
 			}
 		}
 		size++;
+		
 	}
-
-	public Object get(int index) {
-		Node current = head;
+	/**
+	 * in this method we clear the linkedlist
+	 */
+	@Override
+	public void clear() {
+		head=null;
+		size=0;
+		
+	}
+	/**
+	 * in this method we check is the linkedlist contains
+	 * the object
+	 * @param s a generic type,  the object to checked 
+	 * @return a boolean: true if exist, false otherwise
+	 */
+	@Override
+	public boolean contains(T s) {
+		for(int i=0; i < size; i++){
+			if(get(i).equals(s)){
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	 * getting the head
+	 */
+	@Override
+	public T getHead() {
+		
+		return head.getData();
+	}
+	/**
+	 * getting the tail
+	 */
+	@Override
+	public T getTail() {
+		
+		return get(size-1);
+	}
+	/**
+	 * method in which we use the index to obtain the object needed
+	 * @param index an integer
+	 * @return e an object 
+	 * @throws   IndexOutOfBoundsException : if is out of index 
+	 */
+	@Override
+	public T get(int index) throws IndexOutOfBoundsException {
+		INode<T> current = head;
 		if (index == 0) {
 			return head.getData();
 		} else {
@@ -54,15 +113,46 @@ public class LinkedList {
 				}
 				return current.getData();
 			}
-
 		}
-		return "Index Out Of Bounds";
-
+		return null;
+		
 	}
-	
-	public String remove(int index) throws IndexOutOfBoundsException{
-		Node current = head;
-		Node temp = null;
+	/**
+	 * a method in we search in our linkedlist with the help 
+	 * of the object that should be inside the linkedlist
+	 * @param s any kind of object
+	 * @return an integer as the index of the object
+	 */
+	@Override
+	public int indexOf(T s) {
+		INode<T> current = head;
+		for (int i = 0; i < size; i++) {
+			if (current.getData().equals(s)) {
+				return i;
+			}
+			current = current.getNext();
+		}
+		return -1;
+	}
+	/**
+	 * chech if the linkelist is empty wit the size
+	 * @return a boolean tru if the size equal zero , false otherwise.
+	 */
+	@Override
+	public boolean isEmpty() {
+		 return size == 0;
+	}
+	/**
+	 * a method to remove an object with the index
+	 * @param index an integer
+	 * @return The contents of the element that was removed. 
+	  * @throws   IndexOutOfBoundsException : if is out of index 
+	 */
+	@Override
+	public T remove(int index) throws IndexOutOfBoundsException {
+
+		INode <T> current = head;
+		INode <T> temp = null;
 		if (index == 0) {
 			head = current.getNext();
 			size --;
@@ -77,48 +167,14 @@ public class LinkedList {
 			}
 
 		}
-		return null;
+		return get(index);
 	}
-	
-	public boolean contains(String s){
-		for(int i=0; i < size; i++){
-			if(get(i).equals(s)){
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public int indexOf(String s) {
-		Node current = head;
-		for (int i = 0; i < size ; i++) {
-			if (current.getData().equals(s)) {
-				return i;
-			}
-			current = current.getNext();
-		}
-		return -1;
-	}
-
-	public String getHead() {
-		return head.getData().toString();
-	}
-
-	public String getTail() {
-		return get(size() - 1).toString();
-	}
-
+	/**
+	 * a method that give the size
+	 * @return size an integer
+	 */
+	@Override
 	public int size() {
+	
 		return size;
 	}
-
-	public void clear() {
-		head = null;
-		size = 0;
-	}
-
-	public boolean isEmpty() {
-		return size == 0;
-	}
-
-}
